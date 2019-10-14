b6d7dff5bb1275875e645226c5dbc56acac617ad
Giancarlo <pernudig@gmail.com>
2019-10-14 17:25:10 -0600

    converter only working for width < 256 🔢

--- helper_methods.py
+++ helper_methods.py
@@ -2,6 +2,10 @@ def byte_to_int(num):
     return int.from_bytes(num, byteorder='big')
 
 
+def int_to_byte(num):
+    return bytes([num])
+
+
 def pixel(r, g, b):
     return (byte_to_int(r), byte_to_int(g), byte_to_int(b))
 
